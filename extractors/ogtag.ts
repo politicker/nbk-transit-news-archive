6e995a6cd7931016c0ce67256e365729921f0c30
Harrison <h.borges10592@gmail.com>
2024-08-03 18:18:59 -0400

    rename og tag vars

--- extractors/ogtag.ts
+++ extractors/ogtag.ts
@@ -5,12 +5,12 @@ export const extractFromOGTag = async (url: string): Promise<string[]> => {
 	const html = await res.text()
 	const doc = new DOMParser().parseFromString(html, "text/html")
 
-	const titleMeta = doc.querySelector('meta[property="og:title"]')?.innerHTML
-	const authorMeta = doc.querySelector('meta[property="article:author"]')
+	const title = doc.querySelector('meta[property="og:title"]')?.innerHTML
+	const author = doc.querySelector('meta[property="article:author"]')
 		?.innerHTML
-	const publishedMeta = doc.querySelector(
+	const publishedAt = doc.querySelector(
 		'meta[property="article:published_time"]',
 	)?.innerHTML
 
-	return [titleMeta ?? "", publishedMeta ?? "", authorMeta ?? ""]
+	return [title ?? "", publishedAt ?? "", author ?? ""]
 }
