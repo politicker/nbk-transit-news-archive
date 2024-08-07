358d741cb205168d8386793658bdc48c7ca69c6b
Harrison <h.borges10592@gmail.com>
2024-08-07 16:30:50 -0400

    write scraped data to sheets

--- main.ts
+++ main.ts
@@ -3,22 +3,65 @@ import "jsr:@std/dotenv/load"
 import { stringify } from "jsr:@std/csv"
 import { urls } from "./domains.ts"
 import { extractors } from "./extractors/index.ts"
+import { parse } from "https://deno.land/std@0.84.0/flags/mod.ts"
 
-const alreadySeenDomains = new Set<string>()
+import { readJson } from "https://deno.land/std@0.66.0/fs/read_json.ts"
+import { GoogleSpreadsheet } from "npm:google-spreadsheet"
+import { JWT } from "npm:google-auth-library"
+import { formatDate } from "./utils.ts"
 
-// Iterate through list of URLS from the sheet
-const extractedData = []
-for (const url of urls) {
+const credentials = await readJson("credentials.json") as {
+  client_email: string
+  private_key: string
+}
+
+// Initialize auth - see https://theoephraim.github.io/node-google-spreadsheet/#/guides/authentication
+const serviceAccountAuth = new JWT({
+  email: credentials.client_email,
+  key: credentials.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+})
+
+const doc = new GoogleSpreadsheet(
+  "1aFnO_zJ-9UbbLgQKlRGykKq8syBWqZtOHXmdF9zgUrQ", // harrison test sheet
+  serviceAccountAuth,
+)
+
+const flags = parse(Deno.args)
+// const alreadySeenDomains = new Set<string>()
+
+await doc.loadInfo()
+const sheet = doc.sheetsByTitle["Other "]
+
+interface Row {
+  Media: string // publication (e.g. NY Times)
+  Date: string // date published
+  Byline: string // author
+  Headline: string
+  URL: string
+}
+const rows = await sheet.getRows<Row>()
+
+for (const row of rows) {
+  if (row.get("Byline")) {
+    continue
+  }
+
+  const url = row.get("URL")
   const u = new URL(url)
   const domain = u.hostname
 
-  if (alreadySeenDomains.has(domain)) {
+  if (flags.u && !domain.includes(flags.u)) {
     continue
-  } else {
-    alreadySeenDomains.add(domain)
   }
 
-  console.log(`Extracting data from ${domain}`)
+  // if (alreadySeenDomains.has(domain)) {
+  //   continue
+  // } else {
+  //   alreadySeenDomains.add(domain)
+  // }
+
+  console.log(`${row.rowNumber} - extracting data from ${domain}`)
   let headline = ""
   let publicationDate = ""
   let authors = ""
@@ -28,9 +71,19 @@ for (const url of urls) {
       break
     }
 
-    const [foundHeadline, foundPublicationDate, foundAuthors] = await extractor(
-      url,
-    )
+    let foundHeadline = ""
+    let foundPublicationDate = ""
+    let foundAuthors = ""
+    try {
+      ;[foundHeadline, foundPublicationDate, foundAuthors] = await extractor(
+        url,
+      )
+    } catch (e) {
+      console.error(
+        `${row.rowNumber} - error extracting data from ${domain}`,
+        e,
+      )
+    }
 
     if (!headline && foundHeadline) {
       headline = foundHeadline
@@ -43,30 +96,22 @@ for (const url of urls) {
     }
   }
 
-  if (!headline || !publicationDate || !authors) {
-    console.error(`Failed to extract data from ${domain}`)
-    console.log(url)
-    console.error(
-      "headline:",
-      headline,
-      "publicationDate:",
-      publicationDate,
-      "authors:",
-      authors,
-    )
-    console.log("----------------------------------------")
-    continue
+  row.assign({
+    Headline: headline,
+    Date: formatDate(publicationDate),
+    Byline: authors,
+
+    // These should be left alone, but assign requires us to set them
+    URL: url,
+    Media: row.get("Media"),
+  })
+
+  try {
+    console.log(`${row.rowNumber} - saving row`)
+    await row.save()
+  } catch (e) {
+    console.error(`${row.rowNumber} - error updating row`, e)
   }
 
   console.log("----------------------------------------")
-
-  extractedData.push({
-    url,
-    headline,
-    publicationDate,
-    authors,
-  })
 }
-
-// Write data to CSV
-stringify(extractedData)
