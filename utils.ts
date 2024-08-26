4ded23264faf895e578ce1169d5d8741755ec542
Harrison <h.borges10592@gmail.com>
2024-08-26 12:11:10 -0400

    add google creds util

--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
+import { readJson } from "https://deno.land/std@0.66.0/fs/read_json.ts"
+
 export function formatDate(isoDateString: string): string {
 	const date = new Date(isoDateString)
 	if (isNaN(date.getTime())) {
@@ -10,4 +12,14 @@ export function formatDate(isoDateString: string): string {
 		day: "numeric",
 	}
 	return new Intl.DateTimeFormat("en-US", options).format(date)
+}
+
+interface GoogleCredentials {
+	client_email: string
+	private_key: string
+	project_id: string
+}
+
+export async function loadGoogleCredentials(): Promise<GoogleCredentials> {
+	return await readJson("credentials.json") as GoogleCredentials
 }
